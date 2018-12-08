6da8cdb1b54b35ea237fd799e319f6abd954e9dd
Jose Ramon Martin Jimenez <jose.martin06@hotmail.com>
2018-12-07 23:47:56 -0600

    reportes almost done

--- MrPlumber_Herbalife/Room/Views/viewReportes.py
+++ MrPlumber_Herbalife/Room/Views/viewReportes.py
@@ -5,7 +5,31 @@ from rest_framework import status
 from Room.models import Reportes, ActividadesRealizadas, ActividadAlerta
 from Room.serializers import ReportesSerializer, ActividadesRealizadasSerializer,ActividadAlertaSerializer
 
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+
+def my_custom_sql(id=None):
+    if id is None:
+        with connection.cursor() as cursor:
+            cursor.execute(";")
+            row = dictfetchall(cursor)
+        return row
+    elif id > 0:
+        with connection.cursor() as cursor:
+            cursor.execute(";", [id])
+            row = dictfetchall(cursor)
+        return row
+
 class ReportesData(APIView):
+
+
+
     def post(self, request):
         reporte=Reportes()
         print(request.data)
@@ -33,7 +57,7 @@ class ReportesData(APIView):
             ###################
                 actAlrt = ActividadAlerta()
                 actAlrt.actividadRealizada_id=actR.id
-                actAlrt.foco_id=request.data["foco_id"]
+                actAlrt.foco_id=r["foco_id"]
             ## Checar como lo manda Mario
                 actAlrt.observaciones=request.data["ActAlerta_observaciones"]##
             ##
@@ -53,8 +77,22 @@ class ReportesData(APIView):
 
 
 class ReportesInfo(APIView):
+    def get_object(self, pk):
+        try:
+            dato=Reportes.objects.get(pk=pk)
+            return dato
+        except Reportes.DoesNotExist:
+            raise Http404
+
     def get(self, request, pk):
-        return Response(status=status.HTTP_200_OK)
+        dato=self.get_object(pk)
+        serializer = ReportesSerializer(dato)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        dato=self.get_object(pk)
+        dato.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 '''
 {
   "codigoRoom": 2,
